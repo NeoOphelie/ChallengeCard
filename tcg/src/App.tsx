b952d5e9e4fffae7726e97c219a5ca480d356508
Ophelie <baraophelie@orange.fr>
2024-02-28 16:14:23 +0100

    Ajout du Deck

--- tcg/src/App.tsx
+++ tcg/src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import CardComponent from './components/CardComponent'
+import DeckComposant from './components/DeckComponent'
 
 function App() {
   return (
     <>
       <CardComponent />
+      <DeckComposant />
     </>
   )
 }
