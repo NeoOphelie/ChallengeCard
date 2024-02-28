a4a67f39e5921fee46e7ed323d5c2309e2693eb6
nmalevi <nmalevil@gmail.com>
2024-02-28 15:56:44 +0100

    add edit

--- tcg/src/store/actionCreators.ts
+++ tcg/src/store/actionCreators.ts
@@ -1,9 +1,16 @@
 import * as actionTypes from "./actionTypes"
 
 const initGame: Game = {
-  id: 42,
-  player1 :  Player,
-  player2 : Player,
+  player1 :  {
+    id : "p1",
+    name : "p1",
+    vp : 0
+},
+  player2 : {
+    id : "p2",
+    name : "p2",
+    vp : 0
+},
   deckPlayer1 : Deck,
   deckPlayer2 : Deck
 }
@@ -18,8 +25,17 @@ export const editGame: GameAction = {
     game: initGame
 }
 
-
 export const removeGame: GameAction = {
     type: actionTypes.REMOVE_GAME,
+    game: initGame
+}
+
+export const editVp: GameAction = {
+    type: actionTypes.EDIT_VP,
+    game: initGame
+}
+
+export const editDeck: GameAction = {
+    type: actionTypes.EDIT_DECK,
     game: initGame
 }
--- tcg/src/store/actionTypes.ts
+++ tcg/src/store/actionTypes.ts
@@ -1,3 +1,5 @@
 export const ADD_GAME = "ADD_GAME"
 export const EDIT_GAME = "EDIT_GAME"
 export const REMOVE_GAME = "REMOVE_GAME"
+export const EDIT_VP = "EDIT_VP"
+export const EDIT_DECK = "EDIT_DECK"
