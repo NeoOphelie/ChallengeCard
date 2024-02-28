55728d8f50baf2a890c18049723309ee5d5b96f2
Mehdi <rame3116@gmail.com>
2024-02-28 15:38:18 +0100

    Board

--- tcg/src/App.tsx
+++ tcg/src/App.tsx
@@ -1,11 +1,17 @@
 import { useState } from 'react'
 import './App.css'
 import CardComponent from './components/CardComponent'
+import Board from './components/Board'
 
 function App() {
+  const player1Cards = ['Carte 1', 'Carte 2', 'Carte 3'];
+  const player2Cards = ['Carte A', 'Carte B', 'Carte C'];
+  const player1Points = 10;
+  const player2Points = 15;
   return (
     <>
-      <CardComponent />
+      <Board player1Points={player1Points} player2Points={player2Points} player1Cards={player1Cards} player2Cards={player2Cards}/>
+      {/* <CardComponent /> */}
     </>
   )
 }
